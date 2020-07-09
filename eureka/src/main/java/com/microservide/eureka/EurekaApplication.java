72c41b0b0517e4e33bb5f710bb9a264b32b5cfcd
manansharma18 <manansharma18@gmail.com>
2020-07-09 12:41:07 -0400

    eureka

---
+++ eureka/src/main/java/com/microservide/eureka/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.microservide.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
